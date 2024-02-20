61c2824d6182ee11b22190c5a35aeb2d116199e7
Kalle Engblom <kalle1942@live.se>
2024-02-20 15:22:16 +0100

    Forgot header file lMAO

---
+++ ASTTraversal.h
@@ -0,0 +1,10 @@
+#ifndef ASTTRAVERSAL_H
+#define ASTTRAVERSAL_H
+
+#include "Node.h"
+#include "SymbolTable.hpp"
+
+// Declare the traverseAST function
+void traverseAST(Node* node, SymbolTable& symbolTable);
+
+#endif // ASTTRAVERSAL_H
